6976a76a1e26da21d47c2ad3438639394938e67a
baojiang wang <edwinwangbj@outlook.com>
2018-11-30 17:30:53 +0800

    2018.11.30.17:30

--- Pokemon/pokemon.cpp
+++ Pokemon/pokemon.cpp
@@ -9,9 +9,14 @@ POKEMON::POKEMON()
 	//Def
 	//Hp
 	//AtkInterval
+	//Accuracy
+	//Evasiveness
+	//wType
+	//Skills
+	//SkillCnt
 }
 
-POKEMON::POKEMON(POKEMONKIND xkind, string xname, int xrank, int xexp, int xatk, int xdef, int xhp, int xatki)
+POKEMON::POKEMON(POKEMONKIND xkind, string xname, int xrank, int xexp, int xatk, int xdef, int xhp, int xatki, double xaccuracy, double xevasiveness, WUXINGTYPE xtype, int xskillcnt)
 {
 	Kind = xkind;
 	Name = xname;
@@ -21,10 +26,26 @@ POKEMON::POKEMON(POKEMONKIND xkind, string xname, int xrank, int xexp, int xatk,
 	Def = xdef;
 	Hp = xhp;
 	AtkInterval = xatki;
+	Accuracy = xaccuracy;
+	Evasiveness = xevasiveness;
+	wType = xtype;
+	SkillCnt = xskillcnt;
 }
 
 POKEMON::POKEMON(const POKEMON & PET)
 {
+	Kind = PET.Get_Kind();
+	Name = PET.Get_Name();
+	Rank = PET.Get_Rank();
+	Exp = PET.Get_Exp();
+	Atk = PET.Get_Atk();
+	Def = PET.Get_Def();
+	Hp = PET.Get_Hp();
+	AtkInterval = PET.Get_AtkI();
+	Accuracy = PET.Get_Accuracy();
+	Evasiveness = PET.Get_Evasiveness();
+	wType = PET.Get_Type();
+	SkillCnt = PET.Get_SkillCnt();
 }
 
 POKEMON::~POKEMON()
@@ -33,72 +54,120 @@ POKEMON::~POKEMON()
 
 void POKEMON::Input_Kind(POKEMONKIND xkind)
 {
+	Kind = xkind;
 }
 
 POKEMONKIND POKEMON::Get_Kind()
 {
-	return POKEMONKIND();
+	return Kind;
 }
 
 void POKEMON::Input_Name(string xname)
 {
+	Name = xname;
 }
 
 string POKEMON::Get_Name()
 {
-	return string();
+	return Name;
 }
 
 void POKEMON::Input_Rank(int xrank)
 {
+	Rank = xrank;
 }
 
 int POKEMON::Get_Rank()
 {
-	return 0;
+	return Rank;
 }
 
 void POKEMON::Input_Exp(int xexp)
 {
+	Exp = xexp;
 }
 
 int POKEMON::Get_Exp()
 {
-	return 0;
+	return Exp;
 }
 
 void POKEMON::Input_Atk(int xatk)
 {
+	Atk = xatk;
 }
 
 int POKEMON::Get_Atk()
 {
-	return 0;
+	return Atk;
 }
 
 void POKEMON::Input_Def(int xdef)
 {
+	Def = xdef;
 }
 
 int POKEMON::Get_Def()
 {
-	return 0;
+	return Def;
 }
 
 void POKEMON::Input_Hp(int xhp)
 {
+	Hp = xhp;
 }
 
 int POKEMON::Get_Hp()
 {
-	return 0;
+	return Hp;
 }
 
 void POKEMON::Input_AtkI(int xatki)
 {
+	AtkInterval = xatki;
 }
 
 int POKEMON::Get_AtkI()
 {
-	return 0;
+	return AtkInterval;
+}
+
+void POKEMON::Input_Accuracy(double xaccuracy)
+{
+	Accuracy = xaccuracy;
+}
+
+double POKEMON::Get_Accuracy()
+{
+	return Accuracy;
+}
+
+void POKEMON::Input_Evasiveness(double xevasiveness)
+{
+	Evasiveness = xevasiveness;
+}
+
+double POKEMON::Get_Evasiveness()
+{
+	return Evasiveness;
+}
+
+void POKEMON::Input_Type(WUXINGTYPE xtype)
+{
+	wType = xtype;
+}
+
+WUXINGTYPE POKEMON::Get_Type()
+{
+	return wType;
+}
+
+void POKEMON::Input_SkillCnt(int xskillcnt)
+{
+	SkillCnt = xskillcnt;
+}
+
+int POKEMON::Get_SkillCnt()
+{
+	return SkillCnt;
 }
--- Pokemon/pokemon.h
+++ Pokemon/pokemon.h
@@ -1,25 +1,55 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
+const int SKILL_NUM = 20;//设定一个精灵可以拥有的技能数上限为20
 //POWER:力量型，高攻击...TANK:肉盾型，高生命值...DEFENSIVE:防御型，高防御...AGILE:敏捷型，低攻击间隔
 enum POKEMONKIND
 {
 	POWER, TANK, DEFENSIVE, AGILE
+};
+//五行属性：水 克 火 克 金 克 木 克 土 克 水
+enum WUXINGTYPE
+{
+	SHUI, HUO, JIN, MU, TU
+};
+//技能种类：攻击和防御
+enum SKILLKIND
+{
+	ATTACK, DEFFENCE
+};
+//技能招式类
+class SKILL {
+public:
+	string SkillName;//技能名称
+	int SkillRank;//技能等级
+	int SkillKind;//技能种类。攻击or防御
+	int SkillPower;//技能威力
+	int SkillHit;//技能命中
+	int SkillPP;//Power Point招式点数，剩余可使用该招式的次数
 };
 //宠物基类;;;;;;技能？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
 class POKEMON {
 private:
 	POKEMONKIND	Kind;//种类
 	string Name;//名字
-	int Rank;//等级
+	int Rank;//等级:每个精灵初始等级为1，满级15集
 	int Exp;//经验值
-	int Atk;//攻击力
-	int Def;//防御力
 	int	Hp;//生命值
-	int	AtkInterval;//攻击间隔
+	int	AtkInterval;//攻击间隔：速度，只要速度高出对方1点就能比对方先出手；若速度一样，攻击次序则随机。 
+	//伤害=（（2*等级+10）/250*攻击方攻击力/防御方防御力*技能威力+2）*加成  加成由属性相克造成
+	int Atk;//攻击力：攻击力高的精灵使用物理招式的伤害多
+	int Def;//防御力：防御力高的精灵受到物理招式的伤害少
+	//命中判定：A=招式的命中*攻击方命中率*防御方回避率。产生一个1～255之间的随机数，该随机数小于A时视为命中，否则为失误。
+	double Accuracy;//命中率：命中率越高招式越容易命中
+	double Evasiveness;//闪避率：闪避率越高，受到的招式越不容易命中
+	WUXINGTYPE wType;//五行属性
+	SKILL Skills[SKILL_NUM];//技能列表
+	int SkillCnt;//当前技能数
 public:
 	POKEMON();//默认构造函数
-	POKEMON(POKEMONKIND xkind, string xname, int xrank, int xexp, int xatk, int xdef, int xhp, int xatki);//赋值构造函数
+	POKEMON(POKEMONKIND xkind, string xname, int xrank, int xexp, int xatk, 
+		int xdef, int xhp, int xatki,double xaccuracy,double xevasiveness,
+		WUXINGTYPE xtype, int xskillcnt);//赋值构造函数
 	POKEMON(const POKEMON &PET);//拷贝构造函数
 	~POKEMON();
 	void Input_Kind(POKEMONKIND xkind);
@@ -38,4 +68,14 @@ public:
 	int Get_Hp();
 	void Input_AtkI(int xatki);
 	int Get_AtkI();
+	void Input_Accuracy(double xaccuracy);
+	double Get_Accuracy();
+	void Input_Evasiveness(double xevasiveness);
+	double Get_Evasiveness();
+	void Input_Type(WUXINGTYPE xtype);
+	WUXINGTYPE Get_Type();
+	void Input_SkillCnt(int xskillcnt);
+	int Get_SkillCnt();
+	//增加技能
+	//攻击函数
 };
